ee1cdf5ac736abd42b065fd0c821676a39dd876c
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-16 22:46:55 +0530

    (feat): add debugger launch config

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function App() {
   return (
