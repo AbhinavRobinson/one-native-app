e58febe80e19b622f1003278495d53e43f2ccfe8
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-17 18:47:27 +0530

    (feat): playing with flex

--- App.tsx
+++ App.tsx
@@ -1,26 +1,18 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, View } from "react-native";
-import { useDimensions } from "@react-native-community/hooks";
+import { SafeAreaView, View } from "react-native";
 
 export default function App() {
-  console.log(useDimensions());
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.view}></View>
-    </SafeAreaView>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "column-reverse",
+      }}
+    >
+      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "lightblue", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  view: {
-    backgroundColor: "#333",
-    width: "100%",
-    height: "30%",
-  },
-});
